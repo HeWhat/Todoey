c61fa8278653cfc937a84536ab4cac73b4261bb7
Onco Martin <martin.onco@nbmac0093.local>
2019-03-03 12:52:34 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Onco Martin on 03.03.19.
+//  Copyright © 2019 iteMap. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
